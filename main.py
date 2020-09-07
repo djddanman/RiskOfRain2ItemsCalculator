7b80f0230fb77031e7bf93bd9cce2cb8b34dd33c
djddanman <djddanman@gmail.com>
2020-09-07 12:26:18 -0500

    Start reformatting items json Start json read

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import PySimpleGUI as sg
+import json
 
 values = list()
+AP_round = {"common": {"Armor-Piercing Rounds": {"property": "Damage to Bosses (Percent)", "base_value": 1, "first_item": 0.2, "stack_effect": 0.2, "stack_type": "linear"}}}
+stun_grenade= {'common': {'Stun Grenade': {"property": "Chance to Stun", 'base_value': 0, "first_item": 0.05, "stack_effect": 0.05, "stack_type": "hyperbolic"}}}
 
 def calculate_stack(base_value, first_item, stack_effect, stack_type, n):
     if stack_type == 'linear':
@@ -13,15 +17,34 @@ def calculate_stack(base_value, first_item, stack_effect, stack_type, n):
 
     return value
 
-AP_round = {"common": {"Armor-Piercing Rounds": {"property": "Damage to Bosses (Percent)", "base_value": 1, "first_item": 0.2, "stack_effect": 0.2, "stack_type": "linear"}}}
-stun_grenade= {'common': {'Stun Grenade': {"property": "Chance to Stun", 'base_value': 0, "first_item": 0.05, "stack_effect": 0.05, "stack_type": "hyperbolic"}}}
 
-item = stun_grenade["common"]["Stun Grenade"]
+def get_items_dict():
+    global _items_dict
 
-n = np.arange(0, 50, 1)
-stack_type = item['stack_type']
-for i in n:
-    values.append(calculate_stack(item['base_value'], item['first_item'], item['stack_effect'], item['stack_type'], i))
-plt.plot(n, values)
+    if _items_dict:
+        return _items_dict
 
-plt.show()
+    with open('items.json', 'r') as file:
+        data = json.load(file)
+        items = dict()
+
+        for rarity in data:
+            for item in data[rarity]:
+                items[item[]
+
+
+
+def select_item_gui():
+
+
+
+if __name__ == '__main__':
+    item = stun_grenade["common"]["Stun Grenade"]
+
+    n = np.arange(0, 50, 1)
+    stack_type = item['stack_type']
+    for i in n:
+        values.append(calculate_stack(item['base_value'], item['first_item'], item['stack_effect'], item['stack_type'], i))
+    plt.plot(n, values)
+
+    plt.show()
