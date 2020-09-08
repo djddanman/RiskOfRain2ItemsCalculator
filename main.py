ca75a49df7b00a526a4254221b59e344bd3d414e
djddanman <djddanman@gmail.com>
2020-09-07 23:19:57 -0500

    Add select_item_gui function

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ values = list()
 
 
 def calculate_stack(base_value, first_item, stack_effect, stack_type, n):
+    value = list()
     if stack_type == 'linear':
         value = base_value + (first_item if n > 0 else 0) + (stack_effect * (n - 1) if n > 1 else 0)
     elif stack_type == 'hyperbolic':
@@ -36,17 +37,29 @@ def get_items_dict():
 
 
 def select_item_gui(items_dict):
-    return items_dict['common']['Tougher Times']['Chance to Block Damage']
+
+    select_item_list = list()
+    for rarity in items_dict:
+        for item in items_dict[rarity]:
+            select_item_list.append(item)
+
+    layout = [[sg.Combo(select_item_list),
+               sg.OK()]]
+
+    window = sg.Window('Select Item', layout)
+    event, values = window.read()
+
+    item = values[0]
 
 
 if __name__ == '__main__':
 
     items_dict = get_items_dict()
 
-    item = select_item_gui(items_dict)
+    item, effect = select_item_gui(items_dict)
 
     n = np.arange(0, 50, 1)
-    stack_type = item['stack_type']
+    stack_type = items_dict['common'][item]['stack_type']
     for i in n:
         values.append(
             calculate_stack(item['base_value'], item['first_item'], item['stack_effect'], item['stack_type'], i))
