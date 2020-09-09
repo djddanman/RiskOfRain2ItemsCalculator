37420af5a39f7a7aebe7c4d16d147f340328e855
djddanman <djddanman@gmail.com>
2020-09-08 19:51:10 -0500

    Add limit check to calculation Add GUI to select effect Add plot formatting

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import PySimpleGUI as sg
 import json
-import Item
 from pathlib import Path
 
 _items_dict = dict()
@@ -11,10 +10,12 @@ data = dict()
 values = list()
 
 
-def calculate_stack(base_value, first_item, stack_effect, stack_type, n):
-    value = list()
+def calculate_stack(base_value, first_item, stack_effect, stack_type, n, limit=None):
+    value = int()
     if stack_type == 'linear':
         value = base_value + (first_item if n > 0 else 0) + (stack_effect * (n - 1) if n > 1 else 0)
+        if limit and value > limit:
+            return limit
     elif stack_type == 'hyperbolic':
         value = 1 - 1 / (stack_effect * n + 1)
     elif stack_type == 'exponential':
@@ -37,32 +38,61 @@ def get_items_dict():
 
 
 def select_item_gui(items_dict):
-
     select_item_list = list()
     for rarity in items_dict:
         for item in items_dict[rarity]:
             select_item_list.append(item)
 
-    layout = [[sg.Combo(select_item_list),
+    layout = [[sg.Combo(select_item_list, size=[20, 1], default_value=select_item_list[0]),
                sg.OK()]]
 
     window = sg.Window('Select Item', layout)
     event, values = window.read()
+    window.close()
 
     item = values[0]
 
+    rarity = ''
+    for r in items_dict:
+        if item in items_dict[r]:
+            rarity = r
+            break
+
+    select_effect_list = list()
+    for effect in items_dict[rarity][item]:
+        select_effect_list.append(effect)
+
+    layout = [[sg.Combo(select_effect_list, size=[20, 1], default_value=select_effect_list[0]),
+               sg.OK()]]
+
+    window = sg.Window('Select Effect', layout)
+    event, values = window.read()
+    window.close()
+
+    effect = values[0]
+
+    return item, rarity, effect
+
 
 if __name__ == '__main__':
-
     items_dict = get_items_dict()
 
-    item, effect = select_item_gui(items_dict)
+    item, rarity, effect = select_item_gui(items_dict)
+    item_effect = items_dict[rarity][item][effect]
 
-    n = np.arange(0, 50, 1)
-    stack_type = items_dict['common'][item]['stack_type']
+    upper_bound = item_effect['limit'][1] + 2 if 'limit' in item_effect else 51
+
+    n = np.arange(0, upper_bound, 1)
+    stack_type = item_effect['stack_type']
+
     for i in n:
         values.append(
-            calculate_stack(item['base_value'], item['first_item'], item['stack_effect'], item['stack_type'], i))
+            calculate_stack(item_effect['base_value'], item_effect['first_item'], item_effect['stack_effect'],
+                            item_effect['stack_type'], i, item_effect['limit'][0] if 'limit' in item_effect else None))
+
     plt.plot(n, values)
+    plt.xlabel('Stack Size')
+    plt.ylabel(item_effect['effect'])
+    plt.title(item)
 
     plt.show()
