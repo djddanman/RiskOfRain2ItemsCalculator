c83ccc72f687215d85e5b9aacf23cd9577257ac8
Daniel <djddanman@gmail.com>
2020-09-12 21:21:12 -0500

    Change to lambda functions

--- main.py
+++ main.py
@@ -9,37 +9,39 @@ data = dict()
 values = list()
 
 
-def calculate_stack(base_value, first_item, stack_effect, stack_type, n, limit=None):
-    value = int()
+def calculate_stack_lambda(base_value, first_item, stack_effect, stack_type, limit=None):
+    if not limit:
+        limit = [0, float('inf')]
 
     if stack_type == 'linear':
-        value = base_value + (first_item if n > 0 else 0) + (stack_effect * (n - 1) if n > 1 else 0)
-        if limit and value > limit:
-            return limit
+        effect = lambda i: (np.array(i) < limit[1]) * (
+                base_value + (np.array(i) > 0) * first_item + (np.array(i) > 1) * (
+                stack_effect * (i - 1))) + (np.array(i) >= limit[1]) * limit[0]
 
     elif stack_type == 'hyperbolic':
-        value = 1 - 1 / (stack_effect * n + 1)
+        effect = lambda n: 1 - 1 / (stack_effect * n + 1)
 
     elif stack_type == 'exponential':
-        value = 1 - (1 - stack_effect) ** n
+        effect = lambda n: 1 - (1 - stack_effect) ** n
 
     elif stack_type == 'bandolier':
-        value = 1 - 1 / (1 + n) ** 0.33
+        effect = lambda n: 1 - 1 / (1 + n) ** 0.33
 
     elif stack_type == 'key':
-        net_rarity = 80 + 20 * n + n ** 2
-        common_rarity = 80 / net_rarity
-        uncommon_rarity = n * 20 / net_rarity
-        legendary_rarity = n ** 2 / net_rarity
-        value = [common_rarity, uncommon_rarity, legendary_rarity]
+        common_rarity = lambda i: 80 / (80 + 20 * n + n ** 2) * 100
+        uncommon_rarity = lambda i: n * 20 / (80 + 20 * n + n ** 2) * 100
+        legendary_rarity = lambda i: n ** 2 / (80 + 20 * n + n ** 2) * 100
+        effect = [common_rarity, uncommon_rarity, legendary_rarity]
 
     elif stack_type == 'genesis':
-        value = base_value / ((first_item if n > 0 else 1) + (stack_effect * (n - 1) if n > 1 else 0))
+        effect = lambda n: base_value / (
+                1 - (np.array(n) > 0) * (-1) + (np.array(n) > 0) * first_item + (np.array(n) > 1) * (
+                stack_effect * (n - 1)))
 
     elif stack_type == 'corpsebloom':
-        value = first_item * stack_effect ** (n - 1)
+        effect = lambda n: first_item * stack_effect ** (n - 1)
 
-    return value
+    return effect
 
 
 def get_items_dict():
@@ -103,28 +105,20 @@ if __name__ == '__main__':
     n = np.arange(0, upper_bound, 1)
     stack_type = item_effect['stack_type']
 
-    for i in n:
-        value = calculate_stack(item_effect['base_value'], item_effect['first_item'], item_effect['stack_effect'],
-                                item_effect['stack_type'], i,
-                                item_effect['limit'][0] if 'limit' in item_effect else None)
-        if item == 'Rusted Key':
-            value = [x * 100 for x in value]
-        elif '%' in item_effect['effect']:
-            value = value * 100
-        values.append(value)
-
-    #    plt.ion()
+    values = calculate_stack_lambda(item_effect['base_value'], item_effect['first_item'], item_effect['stack_effect'],
+                                    item_effect['stack_type'],
+                                    item_effect['limit'] if 'limit' in item_effect else None)
 
     if item == 'Rusted Key':
-        plt.plot(n, [i[0] for i in values], 'k-', label='Common')
-        plt.plot(n, [i[1] for i in values], 'g-', label='Uncommon')
-        plt.plot(n, [i[2] for i in values], 'r-', label='Legendary')
+        plt.plot(n, values[0](n), 'k-', label='Common')
+        plt.plot(n, values[1](n), 'g-', label='Uncommon')
+        plt.plot(n, values[2](n), 'r-', label='Legendary')
         plt.legend()
         plt.axis([0, n[-1], 0, 100])
 
     else:
-        plt.plot(n, values)
-        plt.axis([0, n[-1], 0, max(values)])
+        plt.plot(n, values(n))
+        # plt.axis([0, n[-1], 0, max(values)])
 
     plt.xlabel('Stack Size')
     plt.ylabel(item_effect['effect'])
@@ -133,3 +127,4 @@ if __name__ == '__main__':
     plt.show(block=True)
 
 # TODO Finish adding Lunar items
+# TODO End program or go to menu on plot close
