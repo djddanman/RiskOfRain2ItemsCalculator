85990354241340646ee024dc0af0ee9ae0619263
Daniel <djddanman@gmail.com>
2021-08-17 17:08:11 -0500

    Add app.py with example Dash

--- main.py
+++ main.py
@@ -7,12 +7,18 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.graph_objects as go
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
 _items_dict = dict()
 data = dict()
 values = list()
 
 
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+
 # if __name__ == '__main__':
+
 
 
 # TODO Finish adding Lunar items
